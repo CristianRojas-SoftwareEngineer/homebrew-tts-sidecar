f0a22d17d1af27b464b711b754206d2d6ae235c3
tts-sidecar-ci <ci@tts-sidecar.invalid>
2026-07-15 13:17:55 +0000

    Cask: actualizar a v0.7.4

--- Casks/tts-sidecar.rb
+++ Casks/tts-sidecar.rb
@@ -1,6 +1,6 @@
 cask "tts-sidecar" do
-  version "0.7.3"
-  sha256 "94d76c8d10f09eeab5a3078db416287f4dbbedc18e559a716ee15108ef79b751"
+  version "0.7.4"
+  sha256 "d1cfa0e7bdf43d25e8b6306009bca01861cf9c9d14e5d9573534b5a03741bb56"
 
   url "https://github.com/CristianRojas-SoftwareEngineer/TTS-Sidecar/releases/download/v#{version}/tts-sidecar-#{version}-arm64.dmg"
   name "TTS Sidecar"
